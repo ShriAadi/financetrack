cdb7c21482c96d0dc9eca73a9702078f592e4798
ShriAadi <shriaadi7@gmail.com>
2025-12-31 11:40:50 +0530

    Remove server settings from vite.config.ts
    Removed server configuration from Vite config.

--- vite.config.ts
+++ vite.config.ts
@@ -7,9 +7,7 @@ import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: '/financetrack/', 
-  server: {
-    host: "::",
-    port: 8080,
+  
   },
   plugins: [
     react(),
